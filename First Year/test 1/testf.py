2f6014d67e00650f5f05b949d7c44919c61caec4
Scriel-tech <58804735+Scriel-tech@users.noreply.github.com>
2021-11-22 00:21:49 +0200

    Add files via upload

---
+++ First Year/test 1/testf.py
@@ -0,0 +1,14 @@
+# Program to do mathematics
+# Richmond Arthur
+# ARTRIC001
+# 7 March 2017
+
+x= float(input('Enter the value of x: '))#Getting input from user
+y= float(input('Enter the value of y: '))
+z= float(input('Enter the value of z: '))
+
+result = (z/(x+y))**3#Calculation
+
+print('The answer is ', end='')
+print(round(result))
+
