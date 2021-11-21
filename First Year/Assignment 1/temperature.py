dcae0fd7ae6ee547e2841e0cb4ac91a6e4acafa3
Scriel-tech <58804735+Scriel-tech@users.noreply.github.com>
2021-11-21 22:45:56 +0200

    Add files via upload

---
+++ First Year/Assignment 1/temperature.py
@@ -0,0 +1,16 @@
+# This program accepts a temperature given in Fahrenheit and 
+# prints the equivalent in Celsius and in Kelvin.
+# Name: Stephan Jamieson
+# 27th February 2017
+#
+#input_str = input("Enter a temperature in Fahrenheit: ")
+fahrenheit_value = eval(input("Enter a temperature in Fahrenheit:" ,))
+
+#Calculate equivalent celsius value
+Celsius_value = (fahrenheit_value-32)*(5/9)
+
+# Calculate equivalent kelvin value
+kelvin_value = (Celsius_value+273.15)
+
+print(" The temperature in Celsius is:", Celsius_value)
+print("The temperature in Kelvin is:", kelvin_value) 
