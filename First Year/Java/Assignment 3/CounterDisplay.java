c8f00784639565e0a9ce95f94db138986bbcb4f7
Scriel-tech <58804735+Scriel-tech@users.noreply.github.com>
2021-12-01 18:01:56 +0200

    Update CounterDisplay.java

--- First Year/Java/Assignment 3/CounterDisplay.java
+++ First Year/Java/Assignment 3/CounterDisplay.java
@@ -1,7 +1,11 @@
 /* Richmond Arthur
 ARTRIC001
 12 August 2018
-Program to create class counterDisplay */
+Program to create class counterDisplay
+A CounterDisplay object is a counter that cycles over a fixed range. It contains a current value and an upper
+limit, and it provides methods for setting the current value, advancing it, examining it, and obtaining it in
+the form of a 2 digit String.
+*/
 
 public class CounterDisplay
 {
